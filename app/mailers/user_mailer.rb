63d947aea292d35b141933f26e9448460cd9990f
darrickpang <dkypang@yahoo.com>
2021-01-11 17:30:42 -0800

    users mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,35 @@
 class UserMailer < ApplicationMailer
-    default from: 'notifications@example.com'
-    layout 'awesome'
-    
-    def welcome_email
-        @user = params[:user]
-        @url  = 'http://example.com/login'
-        mail(to: @user.email,
-        subject: 'Welcome to My Awesome Site') do |format|
-        format.html { render 'another_template' }
-        format.text { render plain: 'Render text' }
+    before_action { @business, @user = params[:business], params[:user] }
+
+    after_action :set_delivery_options,
+               :prevent_delivery_to_guests,
+               :set_business_headers
+
+    def feedback_message
+    end
+
+    def campaign_message
+    end
+
+    private
+
+    def set_delivery_options
+        # You have access to the mail instance,
+        # @business and @user instance variables here
+        if @business && @business.has_smtp_settings?
+            mail.delivery_method.settings.merge!(@business.smtp_settings)
+        end
+    end
+
+    def prevent_delivery_to_guests
+        if @user && @user.guest?
+            mail.perform_deliveries = false
+        end
+    end
+
+    def set_business_headers
+        if @business
+            headers["X-SMTPAPI-CATEGORY"] = @business.code
+        end
     end
 end
