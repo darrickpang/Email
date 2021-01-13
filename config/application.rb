aefb0990f804a3b32d0275a97a87e6c029f10b9a
darrickpang <dkypang@yahoo.com>
2021-01-12 18:12:32 -0800

    more email stuff

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-#config.action_mailer.preview_path = "#{Rails.root}/lib/mailer_previews"
-#config.action_mailer.perform_caching = true
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,7 +11,8 @@ module Email
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.action_mailer.preview_path = "#{Rails.root}/lib/mailer_previews"
+    config.action_mailer.perform_caching = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
